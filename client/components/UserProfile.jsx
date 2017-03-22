a02ab98765463f235587887bea871f2111194033
dpollan <davidpollan@gmail.com>
2017-03-22 17:35:35 -0500

    Refactor to put Impressions section into table TODO: clean up and style properly

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -5,19 +5,18 @@ class UserProfile extends React.Component {
 
   render () {
     return (
-      <div>
-        <div className="row">
-          <div className="col-xs-3">
-            <img src={this.props.art_url60}></img>
-          </div>
-          <div className="col-xs-4">
-            <p>{this.props.name}</p>
-          </div>
-          <div className="col-xs-5">
-            <div>{this.props.impression}</div>
-          </div>
-        </div>
-      </div>
+      
+      <tr className="entry row">
+        <td className="col-md-2">
+          <img src={this.props.art_url60}></img>
+        </td>
+        <td className="col-md-5">
+          <p>{this.props.name}</p>
+        </td>
+        <td className="col-md-5">
+          <div>{this.props.impression}</div>
+        </td>
+      </tr>  
     )
   }
 }
--- client/components/UserProfileList.jsx
+++ client/components/UserProfileList.jsx
@@ -1,25 +1,30 @@
-class UserProfile extends React.Component {
+class UserProfileList extends React.Component {
   constructor (props) {
     super (props)
     }
 
   render () {
     return (
-      <div>
-        <div className="row">
-          <div className="col-xs-3">
-            <img src={this.props.art_url60}></img>
-          </div>
-          <div className="col-xs-4">
-            <p>{this.props.name}</p>
-          </div>
-          <div className="col-xs-5">
-            <div>{this.props.impression}</div>
-          </div>
-        </div>
-      </div>
+      <tbody className='container-fluid entryList'>
+        <tr className='row'>
+          <th className='col-md-2'>
+            Album
+          </th>
+          <th className="col-md-5">
+            Artist
+          </th>
+          <th className="col-md-5">
+            Impressions
+          </th>
+        </tr>
+        {this.props.allEntries.map( (entry) => {
+          return <UserProfile art_url60={this.props.art_url60} 
+                  name={this.props.name}
+                  impression={this.props.impression} />
+        })}
+      </tbody>
     )
   }
 }
 
-window.UserProfile = UserProfile;
+window.UserProfileList = UserProfileList;
