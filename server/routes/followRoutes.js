8bc124d0faa6062e3276103c7825b65cf77ef70f
Won Jin Chung <chungw51993@gmail.com>
2017-03-22 15:19:04 -0500

    Deleting all relation

--- server/routes/followRoutes.js
+++ server/routes/followRoutes.js
@@ -15,21 +15,22 @@ router.get('/', function(req, res, next) {
         .catch(function(err) {
           res.send(err);
         });
-    })
+    });
 
 });
 
 router.post('/', function(req, res, next) {
   var username = req.cookies.username;
   var followerUsername = req.body.follower;
-  var userId, followId;
+  var userId;
+  var followId;
 
   user.getUser(username)
     .then(function(result) {
       userId = result.id;
       user.getUser(followerUsername)
         .then(function(result) {
-          followId = result.id
+          followId = result.id;
           follower.addFollower(userId, followId)
             .then(function(result) {
               follower.addFollower(followId, userId)
@@ -38,33 +39,36 @@ router.post('/', function(req, res, next) {
                 })
                 .catch(function(err) {
                   console.error(err);
-                })
-            })
-        })
-    })
+                });
+            });
+        });
+    });
 });
 
 router.post('/:follower', function(req, res, next) {
   var username = req.cookies.username;
   var followerUsername = req.params.follower;
   var userId;
+  var followId;
 
   user.getUser(username)
     .then(function(result) {
       userId = result.id;
       user.getUser(followerUsername)
         .then(function(result) {
-          follower.deleteFollower(userId, result.id)
+          followId = result.id;
+          follower.deleteFollower(userId, followId)
             .then(function(result) {
-              res.sendStatus(200);
-            })
-            .catch(function(err) {
-              console.error(err);
+              follower.deleteFollower(followId, userId)
+                .then(function(result) {
+                  res.sendStatus(200);
+                })
+                .catch(function(err) {
+                  console.error(err);
+                });
             });
-        })
-    })
-
-
+        });
+    });
 });
 
 module.exports = router;
