bbfac0f4c717900301ba20a1bb78f247f687ec8e
Won Jin Chung <chungw51993@gmail.com>
2017-03-22 17:13:03 -0500

    Added impress box and adding impression with album

--- client/components/ImpressBox.jsx
+++ client/components/ImpressBox.jsx
@@ -1,6 +1,5 @@
 class ImpressBox extends React.Component {
   constructor (props) {
-    console.log(props);
     super(props);
     this.state = {
       rating: '',
@@ -20,7 +19,7 @@ class ImpressBox extends React.Component {
 
   render() {
     return (
-      <div onChange={this.props.change(this.state.rating, this.state.impression)}>
+      <form onChange={this.props.change(this.state.rating, this.state.impression)}>
         <textarea className="form-control"
                   id="impression"
                   name="impression"
@@ -43,7 +42,7 @@ class ImpressBox extends React.Component {
           <option value={9}>9</option>
           <option value={10}>10</option>
         </select>
-      </div>
+      </form>
 )
   }
 }
