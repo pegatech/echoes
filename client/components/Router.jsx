b33b4fa61e21ef7b56ec929eca62cf6a5de4ff93
Jerry Krusinski <jkruzr@gmail.com>
2017-03-22 19:56:47 -0500

    A bit more router

--- client/components/Router.jsx
+++ client/components/Router.jsx
@@ -2,6 +2,58 @@ class Router extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      username: '',
+      password: '',
+      user: ''
+    };
+  }
+
+  login(username, password) {
+    $.ajax({
+      method: 'POST',
+      url: '/api/users/login',
+      data: {
+        username: username,
+        password: password
+      },
+      success: (data, statusText, xhr) => {
+        this.setState({
+          isAuthenticated: true
+        });
+      }
+    });
+  }
+
+  signup(user, username, password) {
+    $.ajax({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: {
+        user: user,
+        username: username,
+        password: password
+      },
+      success: function() {
+        this.setState({
+          isAuthenticated: true
+        });
+      }
+    });
+  }
+
+  logout() {
+    $.ajax({
+      method: 'POST',
+      url: '/api/users/logout',
+      success: () => {
+        this.setState({
+          isAuthenticated: false
+        });
+      }
+    });
   }
 
   render() {
@@ -12,11 +64,22 @@ class Router extends React.Component {
             <img src="styles/logo.svg"></img>
           </div>
 
-          <Route exact path="/router" component={Landing} />
+          <Route path="/router" component={Landing} />
           <Route path="/router/login" component={Login} />
           <Route path="/router/signup" component={Signup} />
+          <PrivateRoute path="/router/dashboard" component={App} />
         </div>
       </BrowserRouter>
     );
   }
 }
+
+const PrivateRoute = ({ component, path }) => (
+  <Route path={path} render={props => (
+    auth.isAuthenticated ? (
+      React.createElement(component, props)
+    ) : (
+      <Redirect to="/router/login" />
+    )
+  )}/>
+);
